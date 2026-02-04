dd0b449ca175e2d5ef973a3f5f641ea6d4f599de
Valemilk <nicolasimoes@valemilk.com.br>
2026-02-04 17:23:11 -0300

    Corrigir texto do botão na página de visualização pública do lote

--- frontend/src/pages/PublicLote.tsx
+++ frontend/src/pages/PublicLote.tsx
@@ -89,10 +89,10 @@ const PublicLote = () => {
             Imprimir
           </button>
           <button
-            onClick={() => navigate('/')}
+            onClick={() => window.close()}
             className="px-6 py-3 bg-gray-600 text-white rounded-lg hover:bg-gray-700"
           >
-            Fazer Login
+            Fechar
           </button>
         </div>
       </div>
